c4fd5ef09f425bb55bfe27a1bcc6559ad34b1a04
Sergio Montano <sergiomontano@BCs-MacBook-Pro-2.local>
2020-10-12 14:39:30 -0500

    java

---
+++ BC_SKILLS/new/javascript/introtoJAV/JSBasicsExercise/jsbasicExercise.js
@@ -0,0 +1,16 @@
+console.log(21);
+// number, booleans, null, undefined,
+
+// + - * / %
+
+
+console.log(7%4);
+
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+console.log("spider " + "man");
+
+console.log("sergio".length + "montano".length);
+
+console.log("Avengers: Endgame"[16]);
+
